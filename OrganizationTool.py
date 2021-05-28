bbcf2788772ec52c5344bc9175e85aa2b237e262
wangtao <wt@st.gxu.edu.cn>
2021-05-29 00:18:20 +0800

    第一次提交

---
+++ OrganizationTool.py
@@ -0,0 +1,97 @@
+import copy
+import csv
+import os
+
+root_dir = 'original data'
+for file in os.listdir(root_dir):
+
+    file_name = root_dir + "\\" + file
+
+
+    #把practice.csv存储到内存 内存里面叫wt
+    with open(file_name) as wt:
+        #用csv工具的reader（）方法读取内存里的wt创建wt_csv 是row的集合
+        wt_csv = csv.reader(wt)
+
+        headers = ['evevt','time','magnitude', 'e_magnitude', 'upperlimit', 'band', 'instrument', 'telescope', 'source','magnitude_sys','mag_source']
+        path = "finish"
+        # 数据结构
+        rows = [
+
+        ]
+
+        for row in wt_csv:
+            i = 0
+            if row[0] == 'event':
+                rows.append(headers)
+            else:
+                #在这写逻辑
+                everow = copy.deepcopy(headers)
+                for obj in row:
+                    everow[i] = obj
+                    i = i+1;
+                if everow[4] == 'T':
+                    everow[10] = 'limit'
+                else:
+                    everow[10] = 'Ph'
+
+
+                if everow[5] == 'u\'':
+                    everow[5] = 'u'
+                if everow[5] == 'g\'':
+                    everow[5] = 'g'
+                if everow[5] == 'r\'':
+                    everow[5] = 'r'
+                if everow[5] == 'i\'':
+                    everow[5] = 'i'
+                if everow[5] == 'z\'':
+                    everow[5] = 'z'
+                if everow[5] == 'Ks':
+                    everow[5] = 'K'
+                if everow[5] == 'C' or everow[5] == 'w' or everow[5] == 'Clear' or everow[5] == 'clear':
+                    continue
+                if everow[5] == everow[5].lower():
+                    everow[9] = 'AB'
+                if everow[5][0:1].isupper():
+                    everow[9] = 'Vega'
+                if everow[7] == 'Swift' and everow[5] == 'U':
+                    everow[5] = 'u'
+                if everow[7] == 'Swift' and everow[5] == 'M2':
+                    everow[5] = 'UVM2'
+                if everow[7] == 'Swift' and everow[5] == 'W1':
+                    everow[5] = 'UVW1'
+                if everow[7] == 'Swift' and everow[5] == 'W2':
+                    everow[5] = 'UVW2'
+                if everow[6] == 'Swift' and everow[5] == 'U':
+                    everow[5] = 'u'
+                if everow[6] == 'Swift' and everow[5] == 'M2':
+                    everow[5] = 'UVM2'
+                if everow[6] == 'Swift' and everow[5] == 'W1':
+                    everow[5] = 'UVW1'
+                if everow[6] == 'Swift' and everow[5] == 'W2':
+                    everow[5] = 'UVW2'
+                if everow[5] == 'I1' or everow[5] == 'I2' or everow[5] == 'I3' or everow[5] == 'I4':
+                    everow[9] = 'infrared band， need to check the literature'
+                if everow[5] == 'orange':
+                    everow[9] = 'indetermination'
+                if everow[5] == '':
+                    everow[9] = 'Manual hidden'
+                if everow[9] == 'magnitude_sys':
+                    everow[9] = 'Special circumstances need to be reviewed'
+
+
+
+                #把每一行添加到rows里面
+                rows.append(everow)
+
+
+
+        #打开finish\wt.csv文件夹 （w：打开这个文件只用于写入。如果该文件已存在则打开文件，并从开头开始编辑，即原有内容会被删除。如果该文件不存在，创建新文件。）
+        with open(path+'\\'+file,'w',newline='')as wt:
+            #使用csv包内的writer（）工具，写入到wt内
+            #获取写入的对象
+            f_csv = csv.writer(wt)
+            #写表头、写一行
+            #f_csv.writerow(headers)
+            #写入rows对象的内容
+            f_csv.writerows(rows)
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # List-organization-tool
 使用python批量处理超新星观测数据csv文档
+
+本code仅进行了下列任务，未在下列的步骤需要手动完成
+
+1.  在列表尾增加了  mag_source  和  magnitude_sys  两列
+2.  如果  upperlimit  列是F,或者空白， mag_source  列 填Ph 
+3.  如果  upperlimit  列是T，              mag_source  列填limit
+4.  将  band  列 u' 替换为 u ，g' 替换为 g ，r' 替换为 r ，i' 替换为 i ，z' 替换为 z ，Ks 替换为 K 
+5.  如果 band 列出现 C 或者 w 将整行删除
+6.  如果 band 列是小写字母，magnitude_sys 列填 ab
+7.  如果 band 列首字母是大写的，在magnitude_sys列填 Vega
+8   如果 telescop 或者 instrument 是 Swift ，将band列的 U 替换为 u、M2 替换为 UVM2、W1 替换为 UVW1、W2 替换为 UVW2
+9.  如果 band 列是I1，I2，I3，I4，这4种之一  在 magnitude_sys 列 标注出“infrared band， need to check the literature”（红外波段，需要查文献确定星等系统）
+10.  如果 band 列为 orange ，在 magnitude_sys 列 标注出“indetermination”（不确定）交由甘文沛师兄处理
+11.  如果 band 列为 空白 ，在 magnitude_sys 列 标注出 “Manual hidden”（手动隐藏，我不会写这个的code）
+12. 如果 band 列 上述情况都不是，那么在 magnitude_sys 列 标注出 “Special circumstances need to be reviewed”（特殊情况 需要查看）
+
+使用方法
+1.  将甘文沛师兄的批量下载code下载下来的csv文件，粘贴到original data（原始数据）文件夹
+2.  运行程序（wins系统运行UbuntuOrganizationTool.py   Ubuntu系统运行UbuntuOrganizationTool.py）
+3.  结果输出在finish（完成）文件夹
+
+
+2021\5\22
+2021\5\20 第二版（在第八条添加了，如果instrument列是Swift，也要修改波段）
